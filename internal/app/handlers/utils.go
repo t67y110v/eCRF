88ee3e782a1e43cad4e256ce5f99c5164e1db2b0
t67y110v <likogawe@gmail.com>
2023-04-10 20:41:44 +0400

    displaying features

--- internal/app/handlers/utils.go
+++ internal/app/handlers/utils.go
@@ -68,3 +68,16 @@ func passwordError(c *fiber.Ctx) error {
 	c.Cookie(er)
 	return c.RedirectBack("/auth")
 }
+
+func getUserRole(roleId int) string {
+	m := make(map[int]string)
+	m[1] = "Администратор"
+	m[2] = "Исследователь"
+	m[3] = "Главный исследователь"
+	m[4] = "Монитор"
+	m[5] = "Дата Менеджер"
+	m[6] = "Аудитор Контроля Качества"
+	m[7] = "Медицинский монитор"
+	m[8] = "Специалист по фармаконадзору"
+	return m[roleId]
+}
