4dc22882fc0660138fe336e2bbe7059f03ba8a15
t67y110v <likogawe@gmail.com>
2023-05-07 12:58:52 +0400

    subject model upddate

--- internal/app/model/subject/subject_model.go
+++ internal/app/model/subject/subject_model.go
@@ -30,6 +30,22 @@ type Subject struct {
 	CenterID          int                `bson:"center_id" `
 	ProtocolId        int                `bson:"protocol_id"`
 	Initials          string             `bson:"initials"`
+	InformaionConsent struct {
+		Signed                      bool      `bson:"signed"`
+		DateOfSign                  string    `bson:"date_of_sign"`
+		TimeOfSign                  time.Time `bson:"time_of_sign"`
+		ReceivedAnInsurancePolicy   bool      `bson:"received_an_insurance_policy"`
+		ReceivedAnInformaionConsent bool      `bson:"received_an_informaion_consent"`
+	}
+
+	Anthropometry struct {
+		AnthropometricDataBeenMeasured bool   `bson:"anthropometric_data_been_measured"`
+		ReasonIfNot                    string `bson:"reason_if_not"`
+		DateOfStartMeasured            string `bson:"date_of_start_measured"`
+		WeightOfBody                   int    `bson:"weight_of_body"`
+		HightOfBody                    int    `bson:"hight_of_body"`
+		IndexWeigthOfBody              int    `bson:"index_weight_of_body"`
+	}
 	DemographySubject struct {
 		Id        int    `bson:"demography_id"`
 		SubjectId int    `bson:"subject_id"`
@@ -37,4 +53,7 @@ type Subject struct {
 		Race      int    `bson:"race"`
 		BirthDate string `bson:"date"`
 	}
+	CriteriaForInclusionInTheStudy bool `bson:"criteria_for_inclusion_in_the_study"`
+
+	ExclusionСriteria bool `bson:"exclusion_criteria"`
 }
