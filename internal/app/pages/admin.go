48238dcf33e525b0f63886c2da9f04e69f90c4ec
t67y110v <likogawe@gmail.com>
2023-05-12 03:28:52 +0400

    admin page routing

--- internal/app/pages/admin.go
+++ internal/app/pages/admin.go
@@ -1,6 +1,8 @@
 package pages
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	model "github.com/t67y110v/web/internal/app/model/user"
 	"github.com/t67y110v/web/internal/app/utils"
@@ -9,6 +11,9 @@ import (
 func (h *Pages) AdminPage() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		user := c.Locals("user").(*model.User)
+		if user.Role != 1 {
+			return utils.ErrorPage(c, errors.New("у вас нет прав для посещения страницы администратора"))
+		}
 		cName, err := h.pgStore.Repository().GetCenterName(user.CenterID)
 		if err != nil {
 			return utils.ErrorPage(c, err)
