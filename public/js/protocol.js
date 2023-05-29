0d8c29bf6bf74d43e4ae2572f91805938270f1d3
t67y110v <likogawe@gmail.com>
2023-05-29 13:22:48 +0400

    demography and informaion criteria looking pretty

--- public/js/protocol.js
+++ public/js/protocol.js
@@ -29,11 +29,11 @@ function updateColorWithComment(id, color) {
 
 function updateField(id, color) {
     let protocolId = document.getElementById("protocol_id_for_js").getAttribute("value")
-    let subjectNum  = document.getElementById("subject_id_for_js").getAttribute("value")
+    let subjectNum = document.getElementById("subject_id_for_js").getAttribute("value")
     let t = document.getElementById("condition" + id)
     let field = t.getAttribute("field-name")
 
-    console.log(subjectNum, protocolId, field ,document.getElementById('new_value_' + id).value )
+    console.log(subjectNum, protocolId, field, document.getElementById('new_value_' + id).value)
     fetch('/subject/screening/updatefield', {
         method: 'POST',
         headers: {
@@ -45,7 +45,7 @@ function updateField(id, color) {
             "field_name": field,
             "value": document.getElementById('new_value_' + id).value,
             "color": color,
-            "subject_number": subjectNum, 
+            "subject_number": subjectNum,
             "protocol_id": protocolId,
         })
 
@@ -77,7 +77,8 @@ function updateSecondColor(id, color) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-             "field_name": field, "value": color, "subject_number": subjectNum, "protocol_id": protocolId })
+            "field_name": field, "value": color, "subject_number": subjectNum, "protocol_id": protocolId
+        })
     })
 
 
@@ -92,10 +93,12 @@ function updateSecondColor(id, color) {
     let protocolId = document.getElementById("protocol_id_for_js").getAttribute("value")
     let subjectNum = document.getElementById("subject_id_for_js").getAttribute("value")
 
+
     for (let i = 1; i <= 57; i++) {
         let t = document.getElementById("condition" + i)
         let color = t.getAttribute("color-value")
-
+        let input_type = t.getAttribute("input-type")
+        let field_name = t.getAttribute("field-name")
         switch (color) {
             case "1":
                 t.insertAdjacentHTML('beforeend', `
@@ -309,7 +312,7 @@ function updateSecondColor(id, color) {
                                  <li > 
                                  <div class="d-grid gap-2">
                                     <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#new_value${i}">
+                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#new_value${i}">
                                      Изменить
                                      </button>
                                     
@@ -328,8 +331,52 @@ function updateSecondColor(id, color) {
               <span title="Валидация монитором" class="input-group-text"
                       id="basic-addon1">⚪</span>
             `)
+                switch (input_type) {
 
-                t.insertAdjacentHTML('beforeend', `
+                    case "select": t.insertAdjacentHTML('beforeend', `
+                <!-- Modal -->
+              <div class="modal fade" id="new_value${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                  aria-labelledby="new_value${i}" aria-hidden="true">
+                  <div class="modal-dialog modal-xl">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h5 class="modal-title" id="new_value${i}">Изменение значения
+                              </h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <div class="row justify-content-center mt-3">
+                          
+                                  <div class="m-3">
+                                      
+                                      <div class="mb-2">
+                                          <label class="form-label">Новое значение:</label>
+                                          <select id="new_value_${i}" name="${field_name}" class="form-select">
+                                       
+                                                 <option value="1">Да</option>
+                                                <option value="2">Нет</option>
+                                          </select>
+                                      </div>
+
+                                      <div class="d-grid gap-2">
+                                          <br>
+                                          <button type="button" onclick="updateField(${i},${1})"  class="btn  btn-outline-success " data-bs-dismiss="modal">Сохранить</button>
+                                      </div>
+                                  </div>
+                      
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Закрыть</button>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>`
+                    )
+
+
+
+                    default: t.insertAdjacentHTML('beforeend', `
                                       <!-- Modal -->
                                     <div class="modal fade" id="new_value${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
                                         aria-labelledby="new_value${i}" aria-hidden="true">
@@ -347,7 +394,7 @@ function updateSecondColor(id, color) {
                                                             
                                                             <div class="mb-2">
                                                                 <label class="form-label">Новое значение:</label>
-                                                                <input id="new_value_${i}" type="text" class="form-control" name="new_value">
+                                                                <input id="new_value_${i}" type="${input_type}" class="form-control" name="new_value">
                                                             </div>
 
                                                             <div class="d-grid gap-2">
@@ -363,7 +410,10 @@ function updateSecondColor(id, color) {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>`)
+                                    </div>`
+                    )
+                }
+
 
                 continue;
             case "4":
@@ -383,24 +433,17 @@ function updateSecondColor(id, color) {
                              </button>
                         <ul class="dropdown-menu">
                             
+                     
+                        <li > 
+                        <div class="d-grid gap-2">
+                           <!-- Button trigger modal -->
+                           <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#new_value${i}">
+                            Изменить
+                            </button>
                            
-                                 <li ><div class="d-grid gap-2">
-
-                                <button type="button" onclick="updateSecondColor(${i},${5})"  class="btn btn-outline-success">Принять</button>
-
-                            </div></li>
-
-                                 <li > <div class="d-grid gap-2">
-                                    <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#denied_${i}">
-                                     Отклонить
-                                     </button>
-                                    
-
-                                        
-
-                            </div>
-                            </li>
+                   </div>
+                   
+                   </li>
                 
                          </ul>
                     </div>
@@ -408,14 +451,58 @@ function updateSecondColor(id, color) {
                       
                       </span>
             `)
-                t.insertAdjacentHTML('beforeend', `
+                switch (input_type) {
+
+                    case "select": t.insertAdjacentHTML('beforeend', `
+                <!-- Modal -->
+              <div class="modal fade" id="new_value${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                  aria-labelledby="new_value${i}" aria-hidden="true">
+                  <div class="modal-dialog modal-xl">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h5 class="modal-title" id="new_value${i}">Изменение значения
+                              </h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <div class="row justify-content-center mt-3">
+                          
+                                  <div class="m-3">
+                                      
+                                      <div class="mb-2">
+                                          <label class="form-label">Новое значение:</label>
+                                          <select id="new_value_${i}" name="${field_name}" class="form-select">
+                                                <option value="1">Да</option>
+                                                <option value="2">Нет</option>
+                                        </select>
+                                      </div>
+
+                                      <div class="d-grid gap-2">
+                                          <br>
+                                          <button type="button" onclick="updateField(${i},${1})"  class="btn  btn-outline-success " data-bs-dismiss="modal">Сохранить</button>
+                                      </div>
+                                  </div>
+                      
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Закрыть</button>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>`
+                    )
+
+
+
+                    default: t.insertAdjacentHTML('beforeend', `
                                       <!-- Modal -->
-                                    <div class="modal fade" id="denied_${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                                        aria-labelledby="denied_${i}" aria-hidden="true">
+                                    <div class="modal fade" id="new_value${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                                        aria-labelledby="new_value${i}" aria-hidden="true">
                                         <div class="modal-dialog modal-xl">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="denied_${i}">Причина отклонения
+                                                    <h5 class="modal-title" id="new_value${i}">Изменение значения
                                                     </h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
@@ -423,36 +510,15 @@ function updateSecondColor(id, color) {
                                                     <div class="row justify-content-center mt-3">
                                                 
                                                         <div class="m-3">
-                                                            <div class="m-3">
-                                                                <div class="mb-2">
-                                                                
-                                                                    <input class="form-control" type="hidden" placeholder="${protocolId}"
-                                                                        value="${protocolId}" name="protocol_id" id="protocol_id"
-                                                                        readonly>
-                                                                </div>
-                                                                <div class="mb-2">
-                                                               
-                                                                    <input class="form-control" type="hidden" placeholder="${subjectNum}"
-                                                                        value="${subjectNum}" name="center_id" id="center_id" readonly>
-                                                                </div>
-                                                                <div class="mb-2">
-                                                                  
-                                                                    <input class="form-control" type="hidden" placeholder={{.Subject.Number}}
-                                                                        value={{.Subject.Number}} readonly>
-                                                                </div>
-                                                            </div>
+                                                            
                                                             <div class="mb-2">
-                                                                <label class="form-label">Пожалуйста, укажите причину отказа в верификации:</label>
-                                                                <input id="reason${i}" type="text" class="form-control" name="reason">
-                                                            </div>
-                                                            <div class="mb-2">
-                                                                <label class="form-label">Комментарий:</label>
-                                                                <input id="comment${i}" type="text" class="form-control" name="comment">
+                                                                <label class="form-label">Новое значение:</label>
+                                                                <input id="new_value_${i}" type="${input_type}" class="form-control" name="new_value">
                                                             </div>
 
                                                             <div class="d-grid gap-2">
                                                                 <br>
-                                                                <button type="button" onclick="updateColorWithComment(${i},${4})"  class="btn  btn-outline-success " data-bs-dismiss="modal">Принять</button>
+                                                                <button type="button" onclick="updateField(${i},${1})"  class="btn  btn-outline-success " data-bs-dismiss="modal">Сохранить</button>
                                                             </div>
                                                         </div>
                                             
@@ -463,7 +529,9 @@ function updateSecondColor(id, color) {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>`)
+                                    </div>`
+                    )
+                }
 
                 continue;
             case "5":
